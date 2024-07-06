f631e6fa0bcbde33652f4a87427390059fc999c3
Aditi Vasmatkar <100767751+Aditiv12@users.noreply.github.com>
2024-07-06 16:41:51 +0530

    Update tabs.dart

--- tabs.dart
+++ tabs.dart
@@ -6,6 +6,7 @@ import 'package:mealsapp/screens/meals.dart';
 import 'package:mealsapp/widgets/main_drawer.dart';
 import 'package:mealsapp/providers/fav_provider.dart';
 import 'package:mealsapp/providers/filters_provider.dart';
+import 'package:mealsapp/screens/search.dart';
 
 const kIntialFilters = {
   Filter.glutenFree: false,
@@ -18,9 +19,7 @@ class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  ConsumerState<TabsScreen> createState() {
-    return _TabsScreenState();
-  }
+  _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends ConsumerState<TabsScreen> {
@@ -30,6 +29,12 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     setState(() {
       _selectedPageIndex = index;
     });
+  }
+
+  void _openSearch(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => const SearchScreen()),
+    );
   }
 
   void _setScreen(String identifier) async {
@@ -59,6 +64,12 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () => _openSearch(context),
+          )
+        ],
       ),
       drawer: MainDrawer(
         onSelectScreen: _setScreen,
